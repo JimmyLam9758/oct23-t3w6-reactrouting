a6ede1a251b62d78f8a3803121bf9fc52eca45a0
JimmyLam9758 <JimmyLam9758@gmail.com>
2024-05-11 10:41:21 +1000

    feat: routes

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import AboutPage from './pages/About'
+import ContactPage from './pages/Contact'
+import HomePage from './pages/Home'
+import PageNotFound from './pages/PageNotFound'
+
+function App() {
+  
+
+  return (
+    <>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/about/services' element={<AboutPage />} />
+          <Route path='/contact' element={<ContactPage />} />
+          {/* Need a page for any not-matched route */}
+          <Route path='*' element={<PageNotFound />} /> 
+
+          {/* <Route path='/' element={<HomePage />}>
+            <Route path='about'>
+              <Route path='services' element={<AboutPage />} />
+            </Route>
+            <Route path='contact' element={<ContactPage />} />
+          </Route> */}
+        </Routes>
+    </>
+  )
+}
+
+export default App
